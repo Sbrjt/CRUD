30d277dfc4f47738eba9d3d035a7075c599a8294
Sbrjt <champa.281982@gmail.com>
2024-01-23 14:42:56 +0530

    minnor changes

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -10,64 +10,50 @@ async function login(req, res) {
 		return res.send('Username and password are required.')
 	}
 
-	db.get(
-		'SELECT * FROM users WHERE usr = ?',
-		usr,
-		async (err, foundUser) => {
-			if (err) {
-				return res.send('Error checking user')
-			}
+	db.get('SELECT * FROM users WHERE usr = ?', usr, async (err, foundUser) => {
+		if (err) return res.send('Error')
 
-			if (foundUser) {
-				const passwordMatch = await bcrypt.compare(pwd, foundUser.pwd)
+		if (foundUser) {
+			// check password
+			if (await bcrypt.compare(pwd, foundUser.pwd)) {
+				req.session.authenticated = true
 
-				if (passwordMatch) {
-					req.session.authenticated = true
-					// return res.redirect('/dashboard');
-					return res.send(`Welcome, ${usr}! Explore the dashboard.`)
+				// check if user is admin
+				if (foundUser.isadmin) {
+					req.session.isAdmin = true
 				}
-			}
 
-			res.send("Username or password didn't match. Try again")
+				return res.send(`Welcome, ${usr}! Go to the dashboard.`
+				)
+			}
 		}
-	)
+
+		res.send("Username or password didn't match. Try again")
+	})
 }
 
 async function register(req, res) {
-	let { usr, pwd } = req.body
+	const { usr, pwd } = req.body
 
 	if (!usr || !pwd) {
 		return res.send('Username and password are required.')
 	}
 
-	db.get(
-		'SELECT * FROM users WHERE usr = ?',
-		usr,
-		async (err, existingUser) => {
-			if (err) {
-				return res.send('Error checking existing user')
-			}
+	db.get('SELECT * FROM users WHERE usr = ?', usr, async (err, existingUser) => {
+		if (err) return res.send('Error')
 
-			if (existingUser) {
-				return res.send('Username already taken :(')
-			}
-
-			const hashedPassword = await bcrypt.hash(pwd, 10)
-
-			db.run(
-				'INSERT INTO users (usr, pwd) VALUES (?, ?)',
-				usr,
-				hashedPassword,
-				err => {
-					if (err) {
-						return res.send('Error registering user')
-					}
-
-					res.send('Registration successful. Please login.')
-				}
-			)
+		if (existingUser) {
+			return res.send('Username already taken :(')
 		}
-	)
+
+		const hashedPassword = await bcrypt.hash(pwd, 10)
+
+		db.run('INSERT INTO users VALUES (?, ?, true)', usr, hashedPassword, (err) => {
+			if (err) return res.send('Error')
+
+			res.send('Registration successful. Please login.')
+		})
+	})
 }
 
 export { login, register }
--- controllers/prodCtrl.js
+++ controllers/prodCtrl.js
@@ -3,67 +3,50 @@ import sqlite3 from 'sqlite3'
 const db = new sqlite3.Database('./models/data.db')
 
 function getAllProducts(req, res) {
-	db.all('SELECT * FROM products', (err, products) => {
-		if (err) {
-			return res.send('Error retrieving data')
-		}
-		res.json(products)
+	db.all('SELECT * FROM products', (err, rows) => {
+		if (err) return res.send('Error')
+
+		res.json(rows)
 	})
 }
 
 function getProductById(req, res) {
 	const id = req.query.id
-	db.get('SELECT * FROM products WHERE id = ?', id, (err, product) => {
-		if (err) {
-			return res.send('Error retrieving data')
-		}
 
-		if (!product) {
+	db.get('SELECT * FROM products WHERE id = ?', id, (err, row) => {
+		if (err) return res.send('Error')
+
+		if (!row) {
 			return res.send('Not Found')
 		}
 
-		res.send(product.item)
+		res.send(row.item)
 	})
 }
 
 function addProduct(req, res) {
 	const { id, item } = req.body
 
-	db.get(
-		'SELECT * FROM products WHERE id = ?',
-		id,
-		(err, existingProduct) => {
-			if (err) {
-				return res.send('Error checking for existing product')
-			}
+	db.get('SELECT * FROM products WHERE id = ?', id, (err, row) => {
+		if (err) return res.send('Error')
 
-			if (existingProduct) {
-				return res.send('ID should be unique')
-			}
-
-			db.run(
-				'INSERT INTO products (id, item) VALUES (?, ?)',
-				id,
-				item,
-				err => {
-					if (err) {
-						return res.send('Error adding product')
-					}
-
-					res.send(`Record added: ${id}, ${item}`)
-				}
-			)
+		if (row) {
+			return res.send('ID should be unique')
 		}
-	)
+
+		db.run('INSERT INTO products VALUES (?, ?)', id, item, (err) => {
+			if (err) return res.send('Error')
+
+			res.send(`Record added: ${id}, ${item}`)
+		})
+	})
 }
 
 function deleteProduct(req, res) {
 	const id = req.query.id
 
 	db.run('DELETE FROM products WHERE id = ?', id, function (err) {
-		if (err) {
-			return res.send('Error deleting product')
-		}
+		if (err) return res.send('Error')
 
 		if (this.changes === 0) {
 			return res.send('Not found')
